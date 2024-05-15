3cc5c706858253feaeffd51aece7e39342b67599
Iuri Alves Contarelli <iuri.contarelli@wmtrading.com.br>
2024-05-15 15:22:34 -0300

    16: Banco de Dados H2 e Conectando o Angular na API Spring

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -9,7 +9,7 @@ import { delay, first, tap } from 'rxjs';
 
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'http://localhost:9090/api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
