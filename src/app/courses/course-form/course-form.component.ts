21a9611d367c7beea991d4895a8824ccd236e55e
Iuri Alves Contarelli <iuri.contarelli@wmtrading.com.br>
2024-07-12 08:50:48 -0300

    21: Formulário: API Spring: Criar Curso (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
 import { CoursesService } from '../services/courses.service';
 import { error } from 'console';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,7 +17,8 @@ export class CourseFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location,
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -25,15 +27,21 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
-    this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
-    console.log("can");
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', {duration: 3000});
+    this.onCancel();
   }
 
   private onError() {
-    this.snackBar.open('Erro ao salvar curso.', '', {duration: 3000})
+    this.snackBar.open('Erro ao salvar curso.', '', {duration: 3000});
   }
 
 }
