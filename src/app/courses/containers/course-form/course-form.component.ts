5511016907762aaca5349949dffe50902fe6371a
Iuri Alves Contarelli <iuri.contarelli@wmtrading.com.br>
2024-08-07 08:35:26 -0300

    34: Validações de Formulário no Front-end

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { CoursesService } from '../../services/courses.service';
 import { error } from 'console';
@@ -16,8 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: [''],
+    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+    category: ['', [Validators.required]],
   });
 
   constructor(
@@ -53,6 +53,26 @@ export class CourseFormComponent implements OnInit {
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso.', '', {duration: 3000});
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if(field?.hasError('required')) {
+      return 'Campo obrigatório.';
+    }
+
+    if(field?.hasError('minlength')) {
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if(field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
+      return `Tamanho máximo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo inválido.';
   }
 
 }
